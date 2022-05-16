712b5803e4aa734ea4516aa7791856ef3120f617
aktaz-fesoj <93137763+aktaz-fesoj@users.noreply.github.com>
2022-05-16 12:04:40 +0200

    cast 2 - segmenty

--- idk.py
+++ idk.py
@@ -31,3 +31,53 @@ with open("stops.txt", newline='', encoding="utf-8") as csvfile:
     for row in reader:
         stop = Stop()
         stop.load(row)
+        
+        
+        
+        
+ #část 2 - vytvoření úseků - potřeba otestovat
+
+class StopSegment:
+    def __init__(self):
+        self.stop_1 = stop_1
+        self.stop_2 = stop_2
+        self.occurrences = occurrences
+    
+    @classmethod
+    def loading_segments(cls, stoptime_seznam):
+        stopsegments_dict = {}
+
+        for stop_time in stoptime_seznam:
+            if stop_time.stop_sequence == "1":
+                stop_1 = stop_time.stop
+                continue
+            elif stop_time.stop_sequence == "2":
+                stop_2 = stop_time.stop
+
+            else:
+                #vyšší než první dvě zastávky -> druhá jde za první a nově načetlá následuje jako stop_2
+                stop_1 = stop_2
+                stop_2 = stop_time.stop
+
+                
+            # následující podmínka: seřazení dle id, tak aby stejný úsek byl stejně identifikován v obou směrech
+            if stop_1.id <= stop_2.id:
+                first = stop_1
+                second = stop_2
+            else:
+                first = stop_2
+                second = stop_1
+
+            segment_key = (first.id, second.id)
+
+            if segment_key not in stopsegments_dict:
+                occurrences = 1
+                sgmnt = StopSegment(first, second, occurrences)
+                stopsegments_dict[(segment_key)] = sgmnt
+
+            else:
+                stopsegments_dict[segment_key].occurrences += 1
+
+            return stopsegments_dict
+
+
